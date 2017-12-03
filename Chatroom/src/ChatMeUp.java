d61d63ecf6e9e09578f26724b0b1af6f75bd53b8
jpohlman <!@cwj!@691!@>
2017-12-03 01:44:13 -0600

    Switch to swing

--- Chatroom/src/ChatMeUp.java
+++ Chatroom/src/ChatMeUp.java
@@ -6,27 +6,16 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 /**
  * Created by jpohlman on 12/2/17.
  */
-public class ChatMeUp extends Application {
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-
-        final ComboBox<String> chatroomComboBox = new ComboBox<String>();
-        chatroomComboBox.getItems().addAll("Group 9", "Software Design");
-        chatroomComboBox.setValue("Group 9");
-
-        Scene scene = new Scene(root);
-
-        primaryStage.setTitle("Login Page");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+public class ChatMeUp {
+    public static void main(String[] args){
+        Login frame = new Login();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,150);
+        frame.setVisible(true);
     }
 }
---
+++ Chatroom/src/Login.java
@@ -0,0 +1,132 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class Login extends JFrame{
+    private static String user;
+    private static String password;
+    private User currentEntry;
+    private UserQueries personQueries;
+    private int numberOfEntries = 0;
+    private int currentEntryIndex = 0;
+    private JTextField usernameText = new JTextField("Username");
+    private JTextField passwordText = new JTextField("Password");
+    private JLabel selectLabel = new JLabel("Select Chatroom");
+    private String[] chatroomsArray = {"Software Design", "Group Nine"};
+    private JComboBox<String> chatroomsComboBox = new JComboBox<String>(chatroomsArray);
+    private JButton loginButton = new JButton("Login");
+    private JButton registerButton = new JButton("Register");
+    private JLabel errorLabel = new JLabel("Incorrect Username/Password");
+    private RegisterHandler rHandler = new RegisterHandler();
+    private LoginHandler lHandler = new LoginHandler();
+
+    // initialize chatServer and set up GUI
+    public Login() {
+        super("Login");
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(10, 5, 10, 5);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        add(selectLabel, constraints);
+
+        constraints.gridx = 1;
+        add(chatroomsComboBox, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 4;
+        add(usernameText, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 4;
+        add(passwordText, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 1;
+        loginButton.addActionListener(lHandler);
+        add(loginButton, constraints);
+
+        constraints.gridx = 1;
+        registerButton.addActionListener(rHandler);
+        add(registerButton, constraints);
+
+        setSize(300, 150); // set size of window
+        setVisible(true); // show window
+    } // end Client constructor
+
+    private class LoginHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            List<User> results;
+            user = usernameText.getText();
+            password = passwordText.getText();
+
+            try{
+                personQueries = new UserQueries();
+                results = personQueries.getUser(user, password);
+                numberOfEntries = results.size();
+
+                if (numberOfEntries > 0){
+                    currentEntry = results.get(currentEntryIndex);
+
+                    if (currentEntry.getUsername().equals(user) && currentEntry.getPassword().equals(password)){
+                        Client application; // declare client application
+
+                        application = new Client("chatRoomB;"); // connect to localhost
+
+                        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        application.runClient();
+                    }
+                    else{
+                        errorLabel.setText("Failure - Wrong Username/Password");
+                    }
+                }
+                else{
+                    errorLabel.setText("Failure - Wrong Username/Password");
+                }
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class RegisterHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            int results;
+            user = usernameText.getText();
+            password = passwordText.getText();
+
+            try{
+                personQueries = new UserQueries();
+                results = personQueries.addUser(user, password);
+
+                if (results == 1){
+                    errorLabel.setText("Successful Registration.\nPlease Select Login.");
+                }
+                else{
+                    errorLabel.setText("Error Try Again");
+                }
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
